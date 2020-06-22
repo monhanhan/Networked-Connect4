97a31999ce68e5f7608e54060c9f8a6ed93c3b48
Ryan <Ryan@Alfred>
2020-06-22 12:09:17 -0700

    Initial commit for new program

--- 216-Networked-Connect4/src/ui/Connect4TextConsole.java
+++ 216-Networked-Connect4/src/ui/Connect4TextConsole.java
@@ -1,5 +1,280 @@
 package ui;
 
+import java.util.Scanner;
+
+import core.Connect4;
+import core.Connect4ComputerPlayer;
+import javafx.application.Application;
+
+/**
+ * 
+ * @author Ryan Munin
+ * @version 3.0
+ *
+ */
 public class Connect4TextConsole {
+	/**
+	 * This is main. It calls all other methods in order.
+	 * 
+	 * @param args is unused.
+	 */
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+
+		if (chooseGUI(myScanner)) {
+			Application.launch(Connect4GUI.class);
+			myScanner.close();
+
+		} else
+
+		{
+			playTextGame(myScanner);
+		}
+
+	}
+
+	/**
+	 * This method asks the player if they would like to play with a GUI or
+	 * text-based interface
+	 * 
+	 * @param myScanner is a scanner object
+	 * @return is a boolean representation of whether the player wants to play
+	 *         with a GUI
+	 */
+	private static boolean chooseGUI(Scanner myScanner) {
+		boolean badInput = true;
+		boolean chooseGUI = false;
+
+		while (badInput) {
+			System.out.print(
+					"Would you like to play using a GUI? Press 'G' for GUI or 'T' for text only");
+			System.out.println();
+			System.out.println();
+			System.out.print(">>");
+
+			String myInput = myScanner.next();
+
+			System.out.println();
+			System.out.println();
+
+			// Cleaning string.
+			myInput = myInput.toUpperCase().trim();
+
+			if ((myInput.equals("G")) || (myInput.equals("T"))) {
+				badInput = false;
+				chooseGUI = (myInput.equals("G"));
+			}
+		}
+
+		return chooseGUI;
+	}
+
+	/**
+	 * This handles the logic for playing a text based game of connect4
+	 * 
+	 * @param myScanner scanner
+	 */
+	private static void playTextGame(Scanner myScanner) {
+		// Create a new instance of the game.
+		Connect4 myGame = new Connect4();
+
+		printBoard(myGame);
+		printGameStart();
+
+		char player = 'Q';
+
+		boolean playComputer = playComputer(myScanner);
+
+		if (playComputer) {
+			System.out.println("Start game against computer.");
+		}
+
+		boolean isGameOver = false;
+		boolean victory = false;
+
+		// Until an end game condition is met, swap turns and allow the players
+		// to play
+		// the game.
+		while (!isGameOver) {
+			if (player != 'X') {
+				player = 'X';
+			} else {
+				player = 'O';
+			}
+
+			// This is if statement enables a computer turn if the player has
+			// opted to play
+			// against a computer and if it is the computer's turn.
+			if ((player == 'O') && playComputer) {
+				System.out.println();
+				System.out.println("Computer turn");
+				System.out.println();
+				Connect4ComputerPlayer.takeTurn(myGame, player);
+				System.out.println();
+
+			} else {
+				takeTurn(player, myScanner, myGame);
+
+			}
+
+			// Check end game conditions.
+			victory = myGame.checkVictory();
+			boolean tie = myGame.checkTie();
+			isGameOver = (victory || tie);
+
+			printBoard(myGame);
+
+		}
+
+		// Print results
+		printGameEnd(victory, player);
+
+		// Close down the game.
+		myScanner.close();
+		System.exit(0);
+
+	}
+
+	/**
+	 * This method takes player input for whether or not a player wants to play
+	 * against a computer. The method will continue prompting until it receives
+	 * either a 'C' or a 'P'. Input can be upper or lower case with any number
+	 * of spaces.
+	 * 
+	 * @param myScanner is a scanner object taking input from the console.
+	 * @return is a boolean representing whether the player wants to play
+	 *         against a computer.
+	 */
+	private static boolean playComputer(Scanner myScanner) {
+		boolean badInput = true;
+		boolean playComputer = false;
+
+		while (badInput) {
+			System.out.print(
+					"Enter 'P' if you want to play against another player; enter 'C' to play against computer.");
+			System.out.println();
+			System.out.println();
+			System.out.print(">>");
+
+			String myInput = myScanner.next();
+
+			System.out.println();
+			System.out.println();
+
+			// Cleaning string.
+			myInput = myInput.toUpperCase().trim();
+
+			if ((myInput.equals("C")) || (myInput.equals("P"))) {
+				badInput = false;
+				playComputer = (myInput.equals("C"));
+			}
+		}
+
+		return playComputer;
+
+	}
+
+	/**
+	 * This prints the opening message of the game.
+	 */
+	private static void printGameStart() {
+		System.out.println("Begin Game.");
+
+	}
+
+	/**
+	 * This prints the game board that has been passed from myGame
+	 * 
+	 * @param myGame is a Connect4 object.
+	 */
+	private static void printBoard(Connect4 myGame) {
+		char[][] board = myGame.getBoard();
+		for (char[] subArray : board) {
+			System.out.print("|");
+
+			for (char currChar : subArray) {
+				System.out.print(currChar);
+				System.out.print("|");
+			}
+			System.out.println();
+		}
+
+	}
+
+	/**
+	 * This prints the end of game messages indicating victory/winner or a tie.
+	 * 
+	 * @param victory is a boolean indicating if there has been a victory. If
+	 *                false, that indicates a tie.
+	 * @param player  is the player (X or O) that won the match.
+	 */
+	private static void printGameEnd(boolean victory, char player) {
+		if (victory) {
+			System.out.print("Player ");
+			System.out.print(player);
+			System.out.print(" Won the Game");
+
+		} else {
+			System.out.print("The Game was a Tie!");
+
+		}
+	}
+
+	/**
+	 * This allows players to play their turn. It takes input and does error
+	 * checking for invalid input before adding pieces to the board. If a column
+	 * if full or the move is invalid the player will be prompted again for new
+	 * input.
+	 * 
+	 * @param player    is the player (X or O) who is taking their turn
+	 * @param myScanner is a scanner object taking input from the console.
+	 * @param myGame    is a Connect4 object.
+	 */
+	private static void takeTurn(char player, Scanner myScanner,
+			Connect4 myGame) {
+		System.out.print("Player");
+		System.out.print(player);
+		System.out.print(" - your turn. ");
+
+		boolean validMove = false;
+
+		while (!validMove) {
+			int col = parseInput(myScanner);
+			validMove = myGame.addPiece(col, player);
+		}
+	}
+
+	/**
+	 * This is a helper function for takeTurn that specifically handles input
+	 * and error checking.
+	 * 
+	 * @param myScanner is a scanner object taking input from the console
+	 * @return is the int representation of player input.
+	 */
+	private static int parseInput(Scanner myScanner) {
+		boolean badInput = true;
+		int inInt = -1;
+
+		while (badInput) {
+			System.out.println("Choose a column number from 1-7.");
+
+			String input = myScanner.next();
+			try {
+				inInt = Integer.parseInt(input);
+
+			} catch (Exception notValidInput) {
+				continue;
+
+			}
+			if ((inInt >= 1) && (inInt <= 7)) {
+				inInt = inInt - 1;
+				badInput = false;
+			}
+
+		}
+
+		return inInt;
+
+	}
 
 }
