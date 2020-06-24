53d31ff5c103122b3f50d8cef65d8bfcc7cf6541
Ryan <Ryan@Alfred>
2020-06-23 21:59:14 -0700

    Deleted dead code.

--- 216-Networked-Connect4/src/ui/LaunchConnect4.java
+++ 216-Networked-Connect4/src/ui/LaunchConnect4.java
@@ -1,7 +1,5 @@
 package ui;
 
-import java.util.Scanner;
-
 import javafx.application.Application;
 
 /**
@@ -21,54 +19,6 @@ public class LaunchConnect4 {
 		// This launches the Connect4GUI application.
 		Application.launch(Connect4GUI.class);
 
-		/**
-		 * The code block below is commented out because only the GUI is in use
-		 * for now. Scanner myScanner = new Scanner(System.in);
-		 * 
-		 * if (chooseGUI(myScanner)) { Application.launch(Connect4GUI.class);
-		 * 
-		 * } else
-		 * 
-		 * { Connect4TextConsole newPlayer = new Connect4TextConsole();
-		 * newPlayer.start(); }
-		 */
-
-	}
-
-	/**
-	 * This method asks the player if they would like to play with a GUI or
-	 * text-based interface
-	 * 
-	 * @param myScanner is a scanner object
-	 * @return is a boolean representation of whether the player wants to play
-	 *         with a GUI
-	 */
-	private static boolean chooseGUI(Scanner myScanner) {
-		boolean badInput = true;
-		boolean chooseGUI = false;
-
-		while (badInput) {
-			System.out.print(
-					"Would you like to play using a GUI? Press 'G' for GUI or 'T' for text only");
-			System.out.println();
-			System.out.println();
-			System.out.print(">>");
-
-			String myInput = myScanner.next();
-
-			System.out.println();
-			System.out.println();
-
-			// Cleaning string.
-			myInput = myInput.toUpperCase().trim();
-
-			if ((myInput.equals("G")) || (myInput.equals("T"))) {
-				badInput = false;
-				chooseGUI = (myInput.equals("G"));
-			}
-		}
-
-		return chooseGUI;
 	}
 
 }
